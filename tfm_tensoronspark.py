659128df7d2dd39d26e722af737777a51acafc66
sergiushub <sergio.dmgz@gmail.com>
2018-01-11 18:06:35 +1100

    Add some feature manipulation examples

--- tfm_tensoronspark.py
+++ tfm_tensoronspark.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[32]:
+# In[79]:
 
 
 import numpy as np
@@ -13,12 +13,13 @@ import re
 import sys
 import tarfile
 from subprocess import Popen, PIPE, STDOUT
+
 def run(cmd):
     p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
     return p.stdout.read()
 
 
-# In[33]:
+# In[80]:
 
 
 # All the constants to run this notebook.
@@ -28,15 +29,11 @@ image_file = ""
 num_top_predictions = 5
 DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
 
-IMG_URL = 'hdfs://localhost:9000/data/im1.jpg'
-TAG_URL = 'hdfs://localhost:9000/data/tags/tags1*.txt'
-
-# The number of images to process.
-image_batch_size = 3
-max_content = 1000L
+IMG_URL = 'hdfs://localhost:9000/data/im6*.jpg'
+TAG_URL = 'hdfs://localhost:9000/data/tags/tags6*.txt'
 
 
-# In[34]:
+# In[81]:
 
 
 def clean_img_rdd(x):
@@ -55,116 +52,7 @@ def read_file_index():
     return im.join(tg)
 
 
-# In[35]:
-
-
-class NodeLookup(object):
-    """Converts integer node ID's to human readable labels."""
-
-    def __init__(self, label_lookup_path=None, uid_lookup_path=None):
-        if not label_lookup_path:
-            label_lookup_path = os.path.join(model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
-        if not uid_lookup_path:
-            uid_lookup_path = os.path.join(model_dir, 'imagenet_synset_to_human_label_map.txt')
-        self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
-
-    def load(self, label_lookup_path, uid_lookup_path):
-        """Loads a human readable English name for each softmax node.
-
-        Args:
-          label_lookup_path: string UID to integer node ID.
-          uid_lookup_path: string UID to human-readable string.
-
-        Returns:
-          dict from integer node ID to human-readable string.
-        """
-        if not gfile.Exists(uid_lookup_path):
-            tf.logging.fatal('File does not exist %s', uid_lookup_path)
-        if not gfile.Exists(label_lookup_path):
-            tf.logging.fatal('File does not exist %s', label_lookup_path)
-
-        # Loads mapping from string UID to human-readable string
-        proto_as_ascii_lines = gfile.GFile(uid_lookup_path).readlines()
-        uid_to_human = {}
-        p = re.compile(r'[n\d]*[ \S,]*')
-        for line in proto_as_ascii_lines:
-            parsed_items = p.findall(line)
-            uid = parsed_items[0]
-            human_string = parsed_items[2]
-            uid_to_human[uid] = human_string
-
-        # Loads mapping from string UID to integer node ID.
-        node_id_to_uid = {}
-        proto_as_ascii = gfile.GFile(label_lookup_path).readlines()
-        for line in proto_as_ascii:
-            if line.startswith('  target_class:'):
-                target_class = int(line.split(': ')[1])
-            if line.startswith('  target_class_string:'):
-                target_class_string = line.split(': ')[1]
-                node_id_to_uid[target_class] = target_class_string[1:-2]
-
-        # Loads the final mapping of integer node ID to human-readable string
-        node_id_to_name = {}
-        for key, val in node_id_to_uid.items():
-            if val not in uid_to_human:
-                tf.logging.fatal('Failed to locate: %s', val)
-            name = uid_to_human[val]
-            node_id_to_name[key] = name
-
-        return node_id_to_name
-
-    def id_to_string(self, node_id):
-        if node_id not in self.node_lookup:
-            return ''
-        return self.node_lookup[node_id]
-
-
-def create_graph():
-    """"Creates a graph from saved GraphDef file and returns a saver."""
-    # Creates graph from saved graph_def.pb.
-    with gfile.FastGFile(os.path.join(model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-        _ = tf.import_graph_def(graph_def, name='')
-
-
-def run_inference_on_image(image):
-    """Runs inference on an image.
-
-    Args:
-       image: Image file name.
-
-    Returns:
-        Nothing
-    """
-    if not gfile.Exists(image):
-        tf.logging.fatal('File does not exist %s', image)
-    image_data = gfile.FastGFile(image, 'rb').read()
-
-    # Creates graph from saved GraphDef.
-    create_graph()
-
-    with tf.Session() as sess:
-        # Some useful tensors:
-        # 'softmax:0': A tensor containing the normalized prediction across
-        #   1000 labels.
-        # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
-        #   float description of the image.
-        # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
-        #   encoding of the image.
-        # Runs the softmax tensor by feeding the image_data as input to the graph.
-        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-        predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
-        predictions = np.squeeze(predictions)
-
-        # Creates node ID --> English string lookup.
-        node_lookup = NodeLookup()
-
-        top_k = predictions.argsort()[-num_top_predictions:][::-1]
-        for node_id in top_k:
-            human_string = node_lookup.id_to_string(node_id)
-            score = predictions[node_id]
-            print('%s (score = %.5f)' % (human_string, score))
+# In[82]:
 
 
 def maybe_download_and_extract():
@@ -185,24 +73,24 @@ def maybe_download_and_extract():
         print('Data already downloaded:', filepath, os.stat(filepath))
 
 
-# In[36]:
+# In[83]:
 
 
 maybe_download_and_extract()
 
 
-# In[37]:
+# In[84]:
 
 
 image_data = read_file_index()
-image_data.collect()
 
 
-# In[38]:
+# In[85]:
 
 
 label_lookup_path = os.path.join(model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
 uid_lookup_path = os.path.join(model_dir, 'imagenet_synset_to_human_label_map.txt')
+
 def load_lookup():
     """Loads a human readable English name for each softmax node.
     
@@ -251,13 +139,13 @@ def load_lookup():
 node_lookup = load_lookup()
 
 
-# In[39]:
+# In[86]:
 
 
 node_lookup_bc = sc.broadcast(node_lookup)
 
 
-# In[40]:
+# In[87]:
 
 
 model_path = os.path.join(model_dir, 'classify_image_graph_def.pb')
@@ -265,13 +153,13 @@ with gfile.FastGFile(model_path, 'rb') as f:
     model_data = f.read()
 
 
-# In[41]:
+# In[88]:
 
 
 model_data_bc = sc.broadcast(model_data)
 
 
-# In[45]:
+# In[89]:
 
 
 def run_image(sess, img_id, image, tags, node_lookup):
@@ -290,7 +178,8 @@ def run_image(sess, img_id, image, tags, node_lookup):
         else:
             human_string = node_lookup[node_id]
         score = predictions[node_id]
-        scores.append((human_string, score))
+        #scores.append((human_string, score))
+        scores.append((node_id, score))
     return (img_id, tags, scores)
 
 def apply_batch(image_entry):
@@ -306,16 +195,84 @@ def apply_batch(image_entry):
             return labelled
 
 
-# In[46]:
+# In[90]:
 
 
-labelled_images = image_data.map(apply_batch)
+# filter the images without tags -> x[1][1] are tags
+labelled_images = image_data.filter(lambda x: x[1][1]).map(apply_batch)
 
 
-# In[47]:
+# In[91]:
 
 
 local_labelled_images = labelled_images.collect()
 
 local_labelled_images
+
+
+# In[69]:
+
+
+from pyspark.ml.feature import Word2Vec
+
+# Input data: Each row is a bag of words from a sentence or document.
+documentDF = spark.createDataFrame([
+    ("Hi I heard about Spark".split(" "), ),
+    ("I wish Java could use case classes".split(" "), ),
+    ("Logistic regression models are neat".split(" "), )
+], ["text"])
+
+# Learn a mapping from words to Vectors.
+word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol="text", outputCol="result")
+model = word2Vec.fit(documentDF)
+
+result = model.transform(documentDF)
+for row in result.collect():
+    text, vector = row
+    print("Text: [%s] => \nVector: %s\n" % (", ".join(text), str(vector)))
+
+
+# In[71]:
+
+
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+
+sentenceData = spark.createDataFrame([
+    (0.0, "Hi I heard about Spark"),
+    (0.0, "I wish Java could use case classes"),
+    (1.0, "Logistic regression models are neat")
+], ["label", "sentence"])
+
+tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
+wordsData = tokenizer.transform(sentenceData)
+
+hashingTF = HashingTF(inputCol="words", outputCol="rawFeatures", numFeatures=20)
+featurizedData = hashingTF.transform(wordsData)
+# alternatively, CountVectorizer can also be used to get term frequency vectors
+
+idf = IDF(inputCol="rawFeatures", outputCol="features")
+idfModel = idf.fit(featurizedData)
+rescaledData = idfModel.transform(featurizedData)
+
+rescaledData.select("label", "features").show()
+
+
+# In[78]:
+
+
+from pyspark.ml.feature import CountVectorizer
+
+# Input data: Each row is a bag of words with a ID.
+df = spark.createDataFrame([
+    (0, "a b c d".split(" ")),
+    (1, "a b b c a".split(" "))
+], ["id", "words"])
+
+# fit a CountVectorizerModel from the corpus.
+cv = CountVectorizer(inputCol="words", outputCol="features", vocabSize=4, minDF=2.0)
+
+model = cv.fit(df)
+
+result = model.transform(df)
+result.show(truncate=False)
 
